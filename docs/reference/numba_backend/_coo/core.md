1fe8f0bf983ab6a16e3f7082b35d96d8083ff036
Dea María Léon <deamarialeon@gmail.com>
2024-07-03 14:18:28 +0200

    member COO working

---
+++ docs/reference/numba_backend/_coo/core.md
@@ -0,0 +1,5 @@
+::: sparse.numba_backend._coo.core
+    handler: python
+    options:
+      members:
+        - COO
--- scripts/gen_ref_pages.py
+++ scripts/gen_ref_pages.py
@@ -4,26 +4,26 @@ from pathlib import Path
 
 import mkdocs_gen_files
 
-nav = mkdocs_gen_files.Nav()
+#nav = mkdocs_gen_files.Nav()
 
 root = Path(__file__).parent.parent
 src = root / "sparse"
-# breakpoint()
+#breakpoint()
 for path in sorted(src.rglob("*.py")):
     module_path = path.relative_to(src).with_suffix("")
     doc_path = path.relative_to(src).with_suffix(".md")
     full_doc_path = Path("reference", doc_path)
 
     parts = tuple(module_path.parts)
-    # breakpoint()
+    #breakpoint()
     if parts[-1] == "__init__":
         parts = parts[:-1]
-        doc_path = doc_path.with_name("index.md")
-        full_doc_path = full_doc_path.with_name("index.md")
+        #doc_path = doc_path.with_name("index.md")
+        #full_doc_path = full_doc_path.with_name("index.md")
     elif parts[-1] == "__main__":
         continue
     # breakpoint()
-    nav[parts] = doc_path.as_posix()
+    #nav[parts] = doc_path.as_posix()
 
     with mkdocs_gen_files.open(full_doc_path, "w") as fd:
         identifier = ".".join(parts)
@@ -31,5 +31,5 @@ for path in sorted(src.rglob("*.py")):
 
     mkdocs_gen_files.set_edit_path(full_doc_path, path.relative_to(root))
 
-with mkdocs_gen_files.open("reference/SUMMARY.md", "w") as nav_file:
-    nav_file.writelines(nav.build_literate_nav())
+#with mkdocs_gen_files.open("reference/SUMMARY.md", "w") as nav_file:
+#    nav_file.writelines(nav.build_literate_nav())
